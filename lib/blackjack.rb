6f089cc2b5e0bbcac015ef0fcfaf9ce30623518f
Olga Rosas <olga.rosas@gmail.com>
2019-05-31 21:17:07 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,10 +54,10 @@ end
 
 def runner
   welcome
-  initial_round
+  card_total = initial_round
   until card_total > 21
-    hit?(card_total)
+    card_total = hit?(card_total)
     display_card_total(card_total)
   end
-end_game
+end_game(card_total)
 end
