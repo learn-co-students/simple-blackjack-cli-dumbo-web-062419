f3381c3cc81ce8a1c69c4e1312e551e641ee27a9
Olga Rosas <olga.rosas@gmail.com>
2019-05-31 21:10:57 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,5 +53,11 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  initial_round
+  until card_total> 21
+    hit?(card_total)
+    display_card_total
+  end
+end_game
 end
